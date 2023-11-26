a7ea51dec92cdf2cb0ade3a5a34d32a3a61cfc29
Bruno <Cliente@DESKTOP-2TBK80U>
2023-11-26 15:59:28 -0300

    Implementado os testes de Integração

---
+++ eAgendaMedica.TesteIntegracao/Compartilhado/TestesIntegracaoBase.cs
@@ -0,0 +1,111 @@
+﻿using eAgenda.Dominio.Compartilhado;
+using eAgenda.Dominio.ModuloCirurgia;
+using eAgenda.Dominio.ModuloConsulta;
+using eAgenda.Dominio.ModuloMedico;
+using eAgenda.Infra.Orm.Compartilhado;
+using eAgenda.Infra.Orm.ModuloCirurgia;
+using eAgenda.Infra.Orm.ModuloConsulta;
+using eAgenda.Infra.Orm.ModuloMedico;
+using FizzWare.NBuilder;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace eAgendaMedica.TesteIntegracao.Compartilhado
+{
+    public class TestesIntegracaoBase
+    {
+        protected IRepositorioMedico repositorioMedico;
+        protected IRepositorioConsulta repositorioConsulta;
+        protected IRepositorioCirurgia repositorioCirurgia;
+
+        protected IContextoPersistencia contextoPersistencia;
+
+        public TestesIntegracaoBase()
+        {
+
+            string connectionString = ObterConnectionString();
+
+            var optionsBuilder = new DbContextOptionsBuilder<eAgendaDbContext>();
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            var dbContext = new eAgendaDbContext(optionsBuilder.Options);
+
+            dbContext.Database.Migrate();
+
+            LimparTabelas();
+
+            contextoPersistencia = dbContext;
+
+            repositorioMedico = new RepositorioMedicoOrm(dbContext);
+            repositorioConsulta = new RepositorioConsultaOrm(dbContext);
+            repositorioCirurgia = new RepositorioCirurgiaOrm(dbContext);
+
+
+            BuilderSetup.SetCreatePersistenceMethod<Medico>((Medico) =>
+            {
+                Task.Run(async () =>
+                {
+                    await repositorioMedico.InserirAsync(Medico);
+                    await contextoPersistencia.GravarDadosAsync();
+                }).GetAwaiter().GetResult();
+            });
+
+            BuilderSetup.SetCreatePersistenceMethod<Consulta>((Consulta) =>
+            {
+                Task.Run(async () =>
+                {
+                    await repositorioConsulta.InserirAsync(Consulta);
+                    await contextoPersistencia.GravarDadosAsync();
+                }).GetAwaiter().GetResult();
+            });
+
+            BuilderSetup.SetCreatePersistenceMethod<Cirurgia>((Cirurgia) =>
+            {
+                Task.Run(async () =>
+                {
+                    await repositorioCirurgia.InserirAsync(Cirurgia);
+                    await contextoPersistencia.GravarDadosAsync();
+                }).GetAwaiter().GetResult();
+            });
+        }
+
+        protected static void LimparTabelas()
+        {
+            string? connectionString = ObterConnectionString();
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            string sqlLimpezaTabela =
+                @"
+
+                DELETE FROM [DBO].[TBMEDICOCIRURGIA]
+
+                DELETE FROM [DBO].[TBCIRURGIA]
+                
+                DELETE FROM [DBO].[TBCONSULTA]
+
+                DELETE FROM [DBO].[TBMEDICO];";
+
+            SqlCommand comando = new SqlCommand(sqlLimpezaTabela, sqlConnection);
+
+            sqlConnection.Open();
+
+            comando.ExecuteNonQuery();
+
+            sqlConnection.Close();
+        }
+
+        protected static string ObterConnectionString()
+        {
+            var configuracao = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuracao.GetConnectionString("SqlServer");
+            return connectionString;
+        }
+    }
+}
---
+++ eAgendaMedica.TesteIntegracao/ModuloMedico/RepositorioMedicoOrmTest.cs
@@ -0,0 +1,88 @@
+﻿using eAgenda.Dominio.ModuloMedico;
+using eAgendaMedica.TesteIntegracao.Compartilhado;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace eAgendaMedica.TesteIntegracao.ModuloMedico
+{
+    [TestClass]
+    public class RepositorioMedicoOrmTest : TestesIntegracaoBase
+    {
+        [TestMethod]
+        public async Task Deve_Inserir_Medico()
+        {
+            // Arrange
+            var medico = Builder<Medico>.CreateNew().Build();
+
+            // Act
+            await repositorioMedico.InserirAsync(medico);
+            await contextoPersistencia.GravarDadosAsync();
+
+            // Assert
+            var medicoSelecionado = await repositorioMedico.SelecionarPorIdAsync(medico.Id);
+            medicoSelecionado.Should().BeEquivalentTo(medico);
+        }
+
+        [TestMethod]
+        public async Task Deve_Editar_Medico()
+        {
+            // Arrange
+            var medicoId = Builder<Medico>.CreateNew().Persist().Id;
+            var medico = await repositorioMedico.SelecionarPorIdAsync(medicoId);
+            medico.Nome = "Bruno";
+
+            // Act
+            repositorioMedico.Editar(medico);
+            await contextoPersistencia.GravarDadosAsync();
+
+            // Assert
+            var medicoEditado = await repositorioMedico.SelecionarPorIdAsync(medico.Id);
+            medicoEditado.Should().BeEquivalentTo(medico);
+        }
+
+        [TestMethod]
+        public async Task Deve_Excluir_Medico()
+        {
+            // Arrange
+            var medico = Builder<Medico>.CreateNew().Persist();
+
+            // Act
+            repositorioMedico.Excluir(medico);
+            await contextoPersistencia.GravarDadosAsync();
+
+            // Assert
+            var medicoResposta = await repositorioMedico.SelecionarPorIdAsync(medico.Id);
+            medicoResposta.Should().BeNull();
+        }
+
+        [TestMethod]
+        public async Task Deve_selecionar_todos_Medico()
+        {
+            // Arrange
+            var medico1 = Builder<Medico>.CreateNew().Persist();
+            var medico2 = Builder<Medico>.CreateNew().Persist();
+
+            // Act
+            var medicos = await repositorioMedico.SelecionarTodosAsync();
+
+            // Assert
+            medicos.Should().NotBeEmpty();
+            medicos.Should().Contain(medico1);
+            medicos.Should().Contain(medico2);
+        }
+
+        [TestMethod]
+        public async Task Deve_selecionar_medico_por_id()
+        {
+            // Arrange
+            var medico = Builder<Medico>.CreateNew().Persist();
+
+            // Act
+            var medicoEncontrado = await repositorioMedico.SelecionarPorIdAsync(medico.Id);
+
+            // Assert
+            medicoEncontrado.Should().Be(medico);
+        }
+    }
+}
